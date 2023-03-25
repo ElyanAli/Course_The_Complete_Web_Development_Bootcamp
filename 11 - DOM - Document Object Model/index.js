c7a0464a80da982733ff1bd29a5fb5b3d5d37a39
Elyan Ali Momin <88707021+ElyanAli@users.noreply.github.com>
2023-03-26 00:48:16 +0500

    DOM Commit
    Module 11

---
+++ 11 - DOM - Document Object Model/index.js
@@ -0,0 +1,18 @@
+// alert("Hello");
+
+// document.querySelector("h1").innerHTML = "Good Bye";
+
+document.querySelector(".name").innerHTML = "James Bond";
+
+document.querySelector(".item a").style.color = "brown";
+
+document.querySelector(".btn").style.backgroundColor = "yellow";
+
+// document.querySelector(".btn").classList.toggle("invisible");
+
+// document.querySelector("#title").classList.add("huge");
+
+// console.log(document.querySelector("h1").textContent);
+
+document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
+
